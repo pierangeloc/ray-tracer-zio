9aa2a237e0e43befcff70b4f36bdb67181221199
Pierangelo Cecchetto <pierangeloc@gmail.com>
2020-02-03 22:41:59 +0100

    moving to ZLayer: normalReflectModule

--- ray-tracer/src/main/scala/io/tuliplogic/raytracer/ops/model/modules/NormalReflectModule.scala
+++ ray-tracer/src/main/scala/io/tuliplogic/raytracer/ops/model/modules/NormalReflectModule.scala
@@ -1,30 +1,27 @@
 package io.tuliplogic.raytracer.ops.model.modules
 
-import io.tuliplogic.raytracer.geometry.affine.ATModule
+import io.tuliplogic.raytracer.geometry.affine.aTModule
 import io.tuliplogic.raytracer.geometry.affine.PointVec.{Pt, Vec}
 import io.tuliplogic.raytracer.ops.model.data.Scene.{Plane, Shape, Sphere}
-import zio.{UIO, ZIO}
+import zio.{Has, UIO, ZIO, ZLayer}
 
-trait NormalReflectModule {
-  val normalReflectModule: NormalReflectModule.Service[Any]
-}
 
 //TODO: test PBT that the angle between reflect and normal, and incident and normal is equal. Also, the 2 vectors should be coplanar
 //TODO: make these operations just shape related operations, because a shape is able to calculate the normal to itself at a given point, and calculate the reflected vector
-object NormalReflectModule {
+object normalReflectModule {
 
-  trait Service[R] {
-    def normal(p: Pt, o: Shape): ZIO[R, Nothing, Vec]
+  trait Service {
+    def normal(p: Pt, o: Shape): UIO[Vec]
 
-    final def reflect(vec: Vec, normal: Vec): ZIO[R, Nothing, Vec] =
+    final def reflect(vec: Vec, normal: Vec): UIO[Vec] =
       (vec - (normal * (2 * (vec dot normal)))).normalized.orDie
   }
 
-  trait Live extends NormalReflectModule {
+  type NormalReflectModule = Has[Service]
 
-    val aTModule: ATModule.Service[Any]
+  val live = ZLayer.fromService[aTModule.Service, Nothing, NormalReflectModule] { aTModule =>
 
-    val normalReflectModule: Service[Any] = new Service[Any] {
+    Has(new Service {
 
       def canonicalNormal(p: Pt, o: Shape): UIO[Vec] = o match {
         case Sphere(_, _) => UIO.succeed(p - Pt(0, 0, 0))
@@ -40,12 +37,11 @@ object NormalReflectModule {
           worldNormal <- aTModule.applyTf(inverseTransposed, objectNormal)
           normalized <- worldNormal.normalized
         } yield normalized).orDie
-    }
+    })
   }
 
-  object > extends NormalReflectModule.Service[NormalReflectModule] {
-    override def normal(p: Pt, o: Shape): ZIO[NormalReflectModule, Nothing, Vec] =
-      ZIO.accessM(_.normalReflectModule.normal(p, o))
-  }
+  def normal(p: Pt, o: Shape): ZIO[NormalReflectModule, Nothing, Vec] =
+    ZIO.accessM(_.get.normal(p, o))
+
 
 }
